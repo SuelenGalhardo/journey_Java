ae6429020be8896e4107f6137a0ba3134e111ba9
SuelenGalhardo <suelenranucci@gmail.com>
2024-08-22 23:19:53 +0200

    add getMapping

--- src/main/java/com/suelengalhardo/planner/trip/TripController.java
+++ src/main/java/com/suelengalhardo/planner/trip/TripController.java
@@ -1,13 +1,16 @@
 package com.suelengalhardo.planner.trip;
 
-
+import java.util.Optional;
+import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 import com.suelengalhardo.planner.participant.ParticipantService;
 
@@ -30,5 +33,10 @@ public class TripController {
         return ResponseEntity.ok(new TripCreateResponse(newTrip.getId()));
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<Trip> getTripDetails(@PathVariable UUID id){
+        Optional<Trip> trip = this.repository.findById(id);
+        return trip.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
 
+    }
 }
